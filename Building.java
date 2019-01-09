db25cd83cca3ba3e942feef1d607d5aad01a633d
Kelly Olivier <kolivier20@my.whitworth.edu>
2019-01-08 23:29:18 -0800

    trying to commit final finished hw 2

---
+++ Building.java
@@ -0,0 +1,104 @@
+package JavaHw_2;
+
+
+import java.util.ArrayList;
+
+/**
+ * Building base class, has a list of occupants, a name and an address
+ * building address allowed to change in case of street name change
+ * occupants of a building are there until removed; occupancy based on working/going to school in the building. Not based on instant that the building is checked
+ * @author Kelly Olivier
+ * @version 1.0
+ */
+public class Building {
+    private String name;
+    private String address;
+    protected ArrayList<Person> occupants = new ArrayList<Person>();;
+
+    /**
+     * building constructor
+     * @param newName name of building
+     * @param newAddress address of building
+     */
+    public Building(String newName, String newAddress)
+    {
+        name = newName;
+        address = newAddress;
+    }
+
+    /**
+     * finds a person by name in the occupants list, assumes all names different
+     * @param personName name of person that is being searched for
+     * @return index of the person whose name was searched for, -1 if not found
+     */
+    public int getOccupantsIndex(String personName)
+    {
+        for(int i = 0; i < occupants.size(); i++)
+        {
+            if(occupants.get(i).getName() == personName)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * adds a person occupant to the occupants list
+     * @param person person object that should be added
+     */
+    public void addOccupant(Person person)
+    {
+        occupants.add(person);
+    }
+
+    /**
+     * removes an occupant at a specific index
+     * @param index index of person object in the occupants array
+     */
+    public void removeOccupant(int index)
+    {
+        occupants.remove(index);
+    }
+
+    /**
+     * allows building name to be changed
+     * @param name new name of building
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * allows building address to be changed
+     * @param address new address of building
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * gets building name
+     * @return building name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * gets building address
+     * @return building address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * gets full current occupants list from building
+     * @return arraylist of occupants
+     */
+    public ArrayList<Person> getOccupants() {
+        return occupants;
+    }
+}
+
---
+++ Person.java
@@ -0,0 +1,96 @@
+package JavaHw_2;
+
+/**
+ * represents a basic person of any age, their name, age, phoneNumber, and the total amount of money they have
+ * no way to change age once set, since people are represented as static in time and as permanently occupying buildings (for clarity)
+ * all persons have a total amount of money
+ * @author Kelly Olivier
+ * @version 1.0
+ */
+public class Person {
+    private String name;
+    private int age;
+    private String phoneNumber;
+    private double totalMoney;
+
+    /**
+     * constructor for person
+     * @param newAge the age of the person
+     * @param newName the name of the person
+     * @param newPhone the person's phone number
+     * @param newMoney the amount of money the person starts with
+     */
+    public Person(int newAge, String newName, String newPhone, double newMoney)
+    {
+        name = newName;
+        age = newAge;
+        phoneNumber = newPhone;
+        totalMoney = newMoney;
+    }
+
+    /**
+     * allows money to be added to the total; does not simply overwrite with a new amount
+     * @param money amount of money to be added
+     */
+    public void addMoney(double money)
+    {
+        totalMoney += money;
+    }
+
+    /**
+     * gets person's age
+     * @return age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * gets person's name
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * gets person's phone number
+     * @return phone number
+     */
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    /**
+     * gets the total money that a person possesses
+     * @return total money
+     */
+    public double getTotalMoney() {
+        return totalMoney;
+    }
+
+    /**
+     * changes a person's phone number
+     * @param phoneNumber new phone number
+     */
+    public void setPhoneNumber(String phoneNumber)
+    {
+        this.phoneNumber = phoneNumber;
+    }
+
+    /**
+     * changes a person's name
+     * @param name new name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * completely overwrites a person's current money with a new total
+     * @param totalMoney new total amount of money to change current money to
+     */
+    public void setTotalMoney(double totalMoney) {
+        this.totalMoney = totalMoney;
+    }
+}
